2d756967859beb05e4ab68b94e84c463f1ebf752
Antonio William <antoniowilliam.ti@gmail.com>
2024-01-10 15:25:34 -0300

    Alterando o carregamento do mapeamento para @RequestMapping("/api/realityshows")

--- src/main/java/com/datapro/apiSinopsers/controller/RealityShowController.java
+++ src/main/java/com/datapro/apiSinopsers/controller/RealityShowController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api-sinopse-reality/realityshows")
+@RequestMapping("/api/realityshows")
 public class RealityShowController {
     @Autowired
     private RealityShowService realityShowService;
