3f0707112e731a5495196e8eb522a8d78f9fc41d
Antonio William <antoniowilliam.ti@gmail.com>
2024-01-10 01:14:06 -0300

    primeiro commit

---
+++ src/main/java/com/datapro/apiSinopsers/ApiSinopsersApplication.java
@@ -0,0 +1,13 @@
+package com.datapro.apiSinopsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSinopsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSinopsersApplication.class, args);
+	}
+
+}
